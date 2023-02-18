2d91525ec7feddc77bd4ba2089d79616586d2179
abdullhfaqih <123357349+abdullhfaqih@users.noreply.github.com>
2023-02-18 11:37:06 +0700

    my own project

---
+++ my own project/main.js
@@ -0,0 +1,7 @@
+let masukkan = document.getElementById("masukkan");
+let namaAnda = document.getElementById("namaAnda");
+let tampilan = document.getElementById("tampilan");
+
+function getNama() {
+  namaAnda.value = masukkan.value;
+}
